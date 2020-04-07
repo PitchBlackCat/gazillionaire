cabf416fcc909cf8bb8dee89fe706f8afce03ec0
JB Hemmes <pitchblackcat@gmail.com>
2020-04-07 03:53:29 +0200

    tinkered with client server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,10 +7,10 @@ import {GameRoom} from "./GameRoom";
 import {Lobby} from './Lobby';
 
 const port = Number(process.env.PORT || 2567);
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const server = http.createServer(app);
 const gameServer = new Server({
